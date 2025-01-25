e12952d79793fd9135b64ea847df90dee14e3811
Peter McGuire <petermcgu@gmail.com>
2025-01-25 12:06:55 -0800

    basic bearer token auth working on hello route

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,21 @@ import { AuthService } from './auth.service';
 import { UserModule } from '../user/user.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [UserModule, PassportModule],
-  providers: [AuthService, LocalStrategy],
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,18 +1,9 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Param,
-  Delete, Patch
-} from "@nestjs/common";
+import { Controller, Get, Post, Body, Param, Patch } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserDto } from './dto/user.dto';
 import { User } from './entities/user.entity';
-import { DeleteResult } from 'typeorm';
-import { MoodDto } from "./dto/mood.dto";
-import { UpdateMoodDto } from "./dto/update-mood.dto";
-import { Mood } from "./entities/mood.entity";
+import { MoodDto } from './dto/mood.dto';
+import { Mood } from './entities/mood.entity';
 
 @Controller('user')
 export class UserController {
@@ -23,28 +14,10 @@ export class UserController {
     return this.userService.create(userDto);
   }
 
-  // @Get()
-  // findAll(): Promise<User[]> {
-  //   return this.userService.findAll();
-  // }
-
   @Get(':id')
   findOneById(@Param('id') id: string): Promise<User | null> {
     return this.userService.findOneById(+id);
   }
-
-  // @Patch(':id')
-  // update(
-  //   @Param('id') id: string,
-  //   @Body() updateUserDto: UpdateUserDto,
-  // ): Promise<User> {
-  //   return this.userService.update(+id, updateUserDto);
-  // }
-
-  // @Delete(':id')
-  // remove(@Param('id') id: string): Promise<DeleteResult> {
-  //   return this.userService.remove(+id);
-  // }
 
   @Patch(':id/moods')
   addMoods(
