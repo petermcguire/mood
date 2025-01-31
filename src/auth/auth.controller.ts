ce2426520c9b8c60785351455b96fff1bd760694
Peter McGuire <petermcgu@gmail.com>
2025-01-31 15:39:19 -0800

    added general guard with public option, also added CORS headers

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Request, Post, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from './local-auth.guard';
 import { AuthService } from './auth.service';
+import { Public } from './constants';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @UseGuards(LocalAuthGuard)
   @Post('/login')
   async login(@Request() req): Promise<any> {
